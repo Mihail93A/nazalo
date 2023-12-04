cb41c02ad06c67cee91628e33885bc67137bdf3d
Mihail93A <strannodan@mail.ru>
2023-12-05 03:49:19 +0500

    123

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -11,6 +11,14 @@ import org.openqa.selenium.Keys;
 public class Tests {
 Mozabrick mozabrick = new Mozabrick();
 
+
+    @Test
+    void CheckingAnEmptyCart() {
+        mozabrick.OpenSite()
+                .ClickOnCart()
+                .StayedOnTheMainPage();
+    }
+
     @Test
     void OpeningTheDeliveryPage() {
         mozabrick.OpenSite()
@@ -18,15 +26,9 @@ Mozabrick mozabrick = new Mozabrick();
                 .TheDeliveryPageHasOpened();
     }
 
-    @Test
-    void CheckingAnEmptyCart() {
-        mozabrick.OpenSite()
-                 .ClickOnCart()
-                 .StayedOnTheMainPage();
-    }
 
     @Test
-    void Test3() {
+    void AddingAnItemToYourCart() {
         mozabrick.OpenSite()
                  .ClickBuyDesigner()
                  .ClickBuyOnTheFirstSet()
@@ -34,25 +36,4 @@ Mozabrick mozabrick = new Mozabrick();
                  .MakeSureWeAreInTheCart();
     }
 
-    @Test
-    void Test4() {
-        new Mozabrick().OpenSite();
-        $(".nav__item:nth-child(1)").click();
-        $(byLinkText("Купить")).click();
-        $(".reviews__button").click();
-        $(".basketPage__list-inner > .basketPage__title").shouldHave(text("КОРЗИНА"));
-        $(".fa-times-circle").click();
-        $("h1").shouldHave(text("Ваша корзина пуста!"));
-    }
-
-    @Test
-    void Test5() {
-        new Mozabrick().OpenSite();
-        $(".header__city-select-open:nth-child(4) > .header__city-select-open-value > .open-window2").click();
-        $("#cityNameField").click();
-        $("#cityNameField").val("Саратов");
-        sleep(5000);
-        $("#cityNameField").sendKeys(Keys.ENTER);
-        $(".header__city-select-open:nth-child(4) > .header__city-select-open-value > .open-window2").shouldHave(text("Саратов"));
-    }
 }
