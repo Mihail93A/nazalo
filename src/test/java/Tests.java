b44da8d513f8ccf7d7187183d68f330498de79f3
Mihail93A <strannodan@mail.ru>
2023-12-05 04:20:20 +0500

    123

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,3 +1,6 @@
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -6,34 +9,50 @@ import static com.codeborne.selenide.Selenide.open;
 
 import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
+
 import org.openqa.selenium.Keys;
+
+
+
 
 public class Tests {
 Mozabrick mozabrick = new Mozabrick();
 
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
 
     @Test
     void CheckingAnEmptyCart() {
+        step("Test1", () -> {
         mozabrick.OpenSite()
                 .ClickOnCart()
                 .StayedOnTheMainPage();
+        });
     }
-
-    @Test
-    void OpeningTheDeliveryPage() {
-        mozabrick.OpenSite()
-                .ClickDelivery()
-                .TheDeliveryPageHasOpened();
-    }
-
 
     @Test
     void AddingAnItemToYourCart() {
+        step("Test2", () -> {
         mozabrick.OpenSite()
-                 .ClickBuyDesigner()
-                 .ClickBuyOnTheFirstSet()
-                 .GoToCart()
-                 .MakeSureWeAreInTheCart();
+                .ClickBuyDesigner()
+                .ClickBuyOnTheFirstSet()
+                .GoToCart()
+                .MakeSureWeAreInTheCart();
+        });
     }
+
+
+    @Test
+    void OpeningTheDeliveryPage() {
+        step("Test3", () -> {
+        mozabrick.OpenSite()
+                .ClickDelivery()
+                .TheDeliveryPageHasOpened();
+        });
+    }
+
 
 }
